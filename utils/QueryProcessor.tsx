2f61e278ad2841f3a2c9924c30b97ca9fbb059c5
savannahxunc <56523076+savannahxunc@users.noreply.github.com>
2025-02-25 10:11:22 -0500

    alg32

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -51,5 +51,21 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("square")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers && numbers.length >= 1) {
+      const num1 = parseInt(numbers[0], 10);
+      return (num1 * num1).toString();
+    }
+  }
+  if (query.toLowerCase().includes("multiplied")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers && numbers.length >= 2) {
+      const num1 = parseInt(numbers[0], 10);
+      const num2 = parseInt(numbers[1], 10);
+      return (num1 * num2).toString();
+    }
+  }
+
   return "";
 }
